1d23f9afa53439384e9754c9cad9088313006ac5
Rafael Fernandes Montes <rafaelmontes92@outlook.com>
2020-11-13 16:06:09 -0300

    Criado atributo complexo familia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=30):
-        self.idade = idade
+    def __init__(self, *familia, nome=None, idade=30, parentesco=None):
         self.nome = nome
+        self.idade = idade
+        self.familia = list(familia)
+        self.parentesco = parentesco
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Rafael', 28)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome, p.idade)
-    p.nome = 'Alice'
-    p.idade = 2
-    print(p.nome, p.idade)
+    alice = Pessoa(nome='Alice', idade=2, parentesco='filha')
+    bella = Pessoa(nome='Bella', idade=0, parentesco='filha')
+    angelica = Pessoa(nome='Angelica', idade=28, parentesco='Esposa')
+    rafael = Pessoa(alice, bella, angelica, nome='Rafael', idade=28, parentesco='Pai')
+    print(Pessoa.cumprimentar(rafael))
+    print(id(rafael))
+    print(rafael.cumprimentar())
+    print(rafael.nome, rafael.idade, rafael.parentesco)
+    for familia in rafael.familia:
+        print(familia.nome, familia.idade, familia.parentesco)
