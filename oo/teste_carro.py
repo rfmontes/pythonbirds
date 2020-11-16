19217c83751aa40c3a992cf1c2f311138cf9e69b
Rafael Fernandes Montes <rafaelmontes92@outlook.com>
2020-11-16 15:33:13 -0300

    Criado o teste_carro

---
+++ oo/teste_carro.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from oo.carro import Motor
+
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        motor.acelerar()
+        self.assertEqual(2, motor.velocidade)
+
+    def teste_frear(self):
+        motor = Motor()
+        motor.acelerar()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(1, motor.velocidade)
